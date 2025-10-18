9c2694d9f8a3257eb8ee792c41755b1491a3b0dd
Pavel Nesterov <pnestero@gmail.com>
2025-10-19 00:19:50 +0500

    Добавление log`ирования для файлов utils и masks

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,15 +1,13 @@
 import json
-from unittest.mock import Mock, mock_open, patch
+from unittest.mock import mock_open, patch
 
-from more_itertools.more import side_effect
-
-from src.external_api import currency_conversion, transactions_sum
+from src.external_api import transactions_sum
 from src.utils import get_transaction
 
 
 def test_currency_conversion(transactions):
-    """<UNK> <UNK> <UNK> <UNK> <UNK> <UNK> <UNK>"""
-    with patch("builtins.open", mock_open(read_data=json.dumps(transactions))) as mock_file:
+    """Тест на конвертацию валют"""
+    with patch("builtins.open", mock_open(read_data=json.dumps(transactions))):
         with patch("requests.get") as mock_requests:
             with patch("os.path.exists") as mock_path_exists:
                 mock_path_exists.return_value = True
@@ -25,7 +23,6 @@ def test_decode_error(mock_open):
     with patch("os.path.exists") as mock_path_exists:
         mock_path_exists.return_value = True
         assert get_transaction(" ") == []
-
 
 
 @patch("builtins.open", side_effect=Exception)
