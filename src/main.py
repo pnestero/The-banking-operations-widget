66cbcb4f8db0225bc985b1fdd732efee605ebedd
Pavel Nesterov <pnestero@gmail.com>
2025-11-04 19:55:55 +0500

    Добавление функции main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,26 @@
+from typing import Any
+
 from src.financial_operations import read_csv_transactions, read_excel_transactions
+from src.generators import filter_by_currency
+from src.processing import filter_by_state, sort_by_date
 from src.utils import get_transaction
 
 
 def main():
-    pass
+    data = file_format("D:/PythonProject/PythonProject_NPR/transactions.csv",
+                       "D:/PythonProject/PythonProject_NPR/transactions_excel.xlsx",
+                       "D:/PythonProject/PythonProject_NPR/data/operations.json")
+    file_format_result = search_operation_status(data)
+    search_operation_status_result = sorting_data_operations(file_format_result)
+    sorting_by_nominal = nominal(search_operation_status_result)
 
 
-def file_format(filename_csv=None, filename_excel=None, file=None):
+def file_format(filename_csv=None, filename_excel=None, file_json=None):
     """
-    Запрос от пользователя
+    Запрос от пользователя из какого формата получить транзакцию
+    :param file_json:
     :param filename_csv:
     :param filename_excel:
-    :param file:
     :return:
     """
     print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
@@ -23,84 +32,111 @@ def file_format(filename_csv=None, filename_excel=None, file=None):
 3. Получить информацию о транзакциях из XLSX-файла\n""").strip().lower()
         if search_file == "1":
             print(f"Для обработки выбран JSON-файл")
-            json_format = get_transaction(file)
-            break
+            result_format = get_transaction(file_json)
+            return result_format
         elif search_file == "2":
             print(f"Для обработки выбран CSV-файл")
-            csv_format = read_csv_transactions(filename_csv)
-            break
+            result_format = read_csv_transactions(filename_csv)
+            return result_format
         elif search_file == "3":
             print(f"Для обработки выбран XLSX-файл")
-            json_format = read_excel_transactions(filename_excel)
-            break
+            result_format = read_excel_transactions(filename_excel)
+            return result_format
         else:
             print(f"Введено неверное значение {search_file}")
             continue
 
-    # Статус банковской операции
+def search_operation_status(data: list[dict]) -> list[dict]:
+    """
+    Фильтрация операций по статусу "STATE"
+    :param data: Из какого формата брать информацию
+    :return: Выборку по EXECUTED, CANCELED или PENDING
+    """
+    if data is None:
+        print("Получены пустые данные")
+        return []
     while True:
-        search_operation_status = input(
+        operation_status = input(
             f"Введите статус, по которому необходимо выполнить фильтрацию.\n"
             f"Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING\n").strip().lower()
 
-        if search_operation_status in ["executed", "canceled", "pending"]:
-            if search_operation_status == "executed":
-                print('Операции отфильтрованы по статусу EXECUTED\n')
-                break
-            elif search_operation_status == "canceled":
-                print('Операции отфильтрованы по статусу CANCELED\n')
-                break
-            elif search_operation_status == "pending":
-                print('Операции отфильтрованы по статусу PENDING\n')
-                break
+        if operation_status in ["executed", "canceled", "pending"]:
+            result_status_operations = filter_by_state(data, operation_status)
+            return result_status_operations
         else:
-            print(f"Статус операции {search_operation_status} недоступен")
+            print(f"Статус операции {operation_status} недоступен")
             continue
 
-    # Сортировка операции по дате да/нет
+def sorting_data_operations(file):
+    """
+    Сортировка данных по дате операции
+    :param file: получение данных из def search_operation_status
+    :return: отсортированные данный по дате
+    """
     while True:
-        sorting_data = input("Отсортировать операции по дате? Да/Нет\n").lower().strip()
-        if sorting_data in ["да"]:
-            print("Сортируется по дате")
-            break # функция по сортировке по дате
-        elif sorting_data in ["нет"]:
-            print("Не сортируется по дате")
+        sorting_data_input = input("Отсортировать операции по дате? Да/Нет\n").lower().strip()
+        if sorting_data_input in ["да"]:
+            sort_ascending_descending = input("Отсортировать по возрастанию или по убыванию?\n").lower()
+            if sort_ascending_descending in ["по возрастанию", "по убыванию"]:
+                if sort_ascending_descending == "по возрастанию":
+                    file = sort_by_date(file, reverse=False)
+                    return file
+                elif sort_ascending_descending == "по убыванию":
+                    file = sort_by_date(file, reverse=True)
+                    return file
+            else:
+                file = sort_by_date(file, reverse=False)
+                print("Неправильно выбрана команда, сортируется по убыванию")
+                return file
+        elif sorting_data_input in ["нет"]:
+            print(f"Не сортируется по дате{file}")
+            return file
         else:
             print("Пожалуйста, введите 'Да' или 'Нет'")
             continue
 
-    # Сортировка по возрастанию или по убыванию?
-    while True:
-        sort_ascending_descending = input("Отсортировать по возрастанию или по убыванию?\n").lower()
-        if sort_ascending_descending in ["по возрастанию", "по убыванию"]:
-            if sort_ascending_descending == "по возрастанию":
-                break   # Функция по возрастанию
-            elif sort_ascending_descending == "по убыванию":
-                break   # Функция по убыванию
-        else:
-            # Функция по убыванию
-            print("Неправильно выбрана команда, сортируется по убыванию")
 
     # Сортировка по номиналу
+
+def nominal(search_operation, currency=None):
+    """
+    Сортировка по номиналу из def sorting_data_operations(file)
+    :param search_operation: Список словарей
+    :param currency: Сортировать по RUB
+    :return:
+    """
+    print(search_operation)
     while True:
         sorting_value = input("Выводить только рублевые транзакции? Да/Нет\n").lower()
         if sorting_value in ["да"]:
-            break  # Функция сортировки по валюте
+            currency = "RUB"
+            sorted_nominal = filter_by_currency(search_operation, currency)
+            sorted_nominal_list = list(sorted_nominal)
+            if not sorted_nominal_list:
+                print("Рублевых транзакций не найдено")
+                return search_operation
+            return sorted_nominal_list
+        elif sorting_value in ["нет"]:
+            return search_operation
         else:
-            print("Не сортируется по валюте")
+            print("Пожалуйста, введите 'Да' или 'Нет'")
+            continue
+
 
     # Сортировка по ключевому слову
+def keyword():
     while True:
         sorting_by_word = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет\n").lower()
         if sorting_by_word in ["да"]:
             keyword = input("Введите слово для поиска в описании: ").strip()
-            break  # Функция сортировки по слову
+            pass  # Функция сортировки по слову
         print("Не сортируется по ключевому слову")
 
     print("Распечатываю итоговый список транзакций...")
-
+    pass
 
 if __name__ == "__main__":
-    file_format("D:/PythonProject/PythonProject_NPR/transactions.csv",
-                "D:/PythonProject/PythonProject_NPR/transactions_excel.xlsx",
-                )
+    start = main()
+    # file_format("D:/PythonProject/PythonProject_NPR/transactions.csv",
+    #             "D:/PythonProject/PythonProject_NPR/transactions_excel.xlsx",
+    #             "D:/PythonProject/PythonProject_NPR/data/operations.json")
--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,23 @@
 def filter_by_state(users_id: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Принимает список словарей и возвращает словарь,
     с ключом по умолчанию "EXECUTED"."""
+    if users_id is None:
+        print("Не передан список словарей")
+        return []
     filter_by_list = []
     for user_id in users_id:
-        if user_id.get("state") == state:
+        if user_id.get("state", "").lower() == state.lower():
             filter_by_list.append(user_id)
         else:
             continue
     return filter_by_list
 
 
-def sort_by_date(users_id: list[dict], decreasing: bool = True) -> list[dict]:
+def sort_by_date(users_id: list[dict], reverse: bool = True) -> list[dict]:
     """Сортировка по дате операции"""
-    sorted_by_list_date = sorted(users_id, key=lambda x: x.get("date", ""), reverse=decreasing)
+    if users_id is None:
+        print("Не передан список словарей")
+        return []
+    sorted_by_list_date = sorted(users_id, key=lambda x: x.get("date", ""), reverse=reverse)
 
     return sorted_by_list_date
